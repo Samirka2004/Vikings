360b3bfea12b86851691068bfa9dd91dd5357135
Samir2004 <sem.kurbatov.00@inbox.ru>
2021-05-02 19:59:24 +0300

    vikings

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
+   $('.menu__btn').on('click', function(){
+      $('.menu__list').toggleClass('menu__list--active');
+      $('.menu__bth').toggleClass('menu__bth--active');
+   });
+
+
+
    $('.heroes__slider-img').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -7,6 +14,15 @@ $(function(){
       asNavFor: '.heroes__slider-text',
       prevArrow:'<button class="slick-btn slick-prev" type="button"><img src="images/left.png" alt="prev"></button>',
       nextArrow:'<button class="slick-btn slick-next" type="button"><img src="images/right.png" alt="next"></button>',
+      responsive: [
+         {
+            breakpoint:769,
+            settings: {
+               arrows:false,
+               dots:true,
+            }
+         }
+      ]
    });
    $('.heroes__slider-text').slick({
       slidesToShow: 1,
